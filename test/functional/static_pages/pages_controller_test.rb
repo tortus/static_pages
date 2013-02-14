b3524fc8e4801bbe1795aad81e6a2215972d42ef
William Makley <william@pioneerstreet.com>
2013-02-14 11:04:21 -0500

    add tests for basic functionality

--- test/functional/static_pages/pages_controller_test.rb
+++ test/functional/static_pages/pages_controller_test.rb
@@ -3,9 +3,16 @@ require 'test_helper'
 module StaticPages
   class PagesControllerTest < ActionController::TestCase
 
-    test "should get show" do
-      get :show
+    test "index" do
+      get :index, :use_route => :static_pages
       assert_response :success
+      assert_template "index"
+    end
+
+    test "page in host application" do
+      get "dummy_page", :use_route => :static_pages
+      assert_response :success
+      assert_template "dummy_page"
     end
 
   end
