c41f99e9cc4950d97ad172ddcbc78fae61222b9d
William Makley <william@pioneerstreet.com>
2013-02-14 15:52:49 -0500

    add empty routes test

---
+++ test/integration/routes_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class RoutesTest < ActionDispatch::IntegrationTest
+
+  # why does this not work?
+  # test "pages routes" do
+  #   assert_recognizes({:controller => "static_pages/pages", :action => "show"}, {:path => "test"})
+  # end
+
+end
