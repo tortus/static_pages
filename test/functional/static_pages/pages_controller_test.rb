6279dbb592ea3011971d5017bac181260ef4109c
William Makley <william@pioneerstreet.com>
2013-02-14 15:53:07 -0500

    make functional tests more correct

--- test/functional/static_pages/pages_controller_test.rb
+++ test/functional/static_pages/pages_controller_test.rb
@@ -3,16 +3,22 @@ require 'test_helper'
 module StaticPages
   class PagesControllerTest < ActionController::TestCase
 
-    test "index" do
+    test "#index renders the index page template" do
       get :index, :use_route => :static_pages
       assert_response :success
       assert_template "index"
     end
 
-    test "page in host application" do
-      get "dummy_page", :use_route => :static_pages
+    test "#show renders the template matching params[:page]" do
+      get :show, :page => "dummy_page", :use_route => :static_pages
       assert_response :success
       assert_template "dummy_page"
+    end
+
+    test "#show raises ActionController::RoutingError if template not found" do
+      assert_raise ActionController::RoutingError do
+        get :show, :page => "nonsense", :use_route => :static_pages
+      end
     end
 
     test "#is_safe? returns false for pages with non-alpha-numeric characters" do
