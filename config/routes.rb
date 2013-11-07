20fcd0bd67f5c8162c7248f994efbee1a65adee2
William Makley <wmakley@tortus.com>
2013-11-07 16:21:29 -0500

    only add GET route for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 StaticPages::Engine.routes.draw do
 
-  match ':page(.:format)' => 'pages#show', :as => :page
+  match ':page(.:format)' => 'pages#show', :as => :page, :via => :get
   root :to => 'pages#index'
 
 end
